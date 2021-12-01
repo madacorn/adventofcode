6a50a0706db95ecbbbe26dae98f6a75b6134e305
AStain <codingstain@gmail.com>
2021-12-01 15:02:32 +0000

    Day1 solution

---
+++ 2021/Day1/Day1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1
+{
+    public class Day1Solution1 : AOCLib.ProblemSolver
+    {
+        protected override string Solution(string input)
+        {
+            List<int> num_inputs = AOCLib.Helper.ConvertStringInputToIntList(input);
+            int count = 0;
+            int previous = num_inputs[0];
+            foreach (int current in num_inputs.Skip(1))
+            {
+                if (previous < current) count++;
+                previous = current;
+            }
+
+            return count.ToString();
+        }
+    }
+
+    public class Day1Solution2 : AOCLib.ProblemSolver
+    {
+        protected override string Solution(string input)
+        {
+            List<int> num_inputs = AOCLib.Helper.ConvertStringInputToIntList(input);
+            int count = 0;
+            int sum = num_inputs[0] + num_inputs[1] + num_inputs[2];
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(num_inputs[0]);
+            q.Enqueue(num_inputs[1]);
+            q.Enqueue(num_inputs[2]);
+            foreach (int current in num_inputs.Skip(3))
+            {
+                int oldSum= sum;
+                sum += (current - q.Dequeue());
+                if (oldSum < sum) count++;
+                q.Enqueue(current);
+            }
+
+            return count.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1Solution1 solution1 = new Day1Solution1();
+            Day1Solution2 solution2 = new Day1Solution2();
+            solution1.Run();
+            solution2.Run();
+        }
+    }
+}
---
+++ AOCLib/AOCLib/ProblemSolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOCLib
+{
+    public abstract class ProblemSolver
+    {
+        public const string INPUT_FILE = "input.txt";
+        public const string OUTPUT_FILE = "output.txt";
+
+        public void Run()
+        {
+            string input = File.ReadAllText(INPUT_FILE);
+
+            string result = Solution(input);
+            
+            File.WriteAllText(this.GetType().Name+OUTPUT_FILE, result);
+        }
+
+        protected abstract string Solution(string input);
+        
+    }
+    public static class Helper
+    {
+        public static List<int> ConvertStringInputToIntList(string s)
+        {
+            string[] arr = s.Split('\n');
+            return arr.Select(x => Int32.Parse(x)).ToList();
+        }
+    }
+}
